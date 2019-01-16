5851e1092f70dd05b208fee01bbed886223c3163
jonsonxu <910829960@qq.com>
2019-01-16 16:39:23 +0800

    xzx

--- app.js
+++ app.js
@@ -11,30 +11,65 @@ app.use(bodyParer.urlencoded({
 }))
 
 const upload = multer({
+  // 文件限制
+  limits: {
+    // 限制文件大小500kb
+    fileSize: 500 * 1000,
+    // 限制文件数量
+    // files: 5
+  },
+  // 自定义文件格式限制
+  fileFilter: (req, file, cb) => {
+    const fileType = file.mimetype.split('/')[1]
+    // 只能为以下格式的图片
+    if (fileType === 'png' || fileType === 'jpg' || fileType === 'jpeg') {
+      cb(null, true);
+    } else {
+      cb(null, false)
+      cb(new Error('只能上传.png或.jpg或.jpeg格式的图片'))
+    }
+  },
+  // 储存
   storage: multer.diskStorage({
     destination: (req, file, cb) => {
+      // 如果没有upload文件夹则自动创建
+      if (!fs.existsSync(path.join(__dirname, './uploads'))) {
+        fs.mkdir('./uploads')
+      }
       cb(null, './uploads/');
     },
     filename: (req, file, cb) => {
-      const changedName = (new Date().getTime()) + '-' + file.originalname;
+      const fileType = file.mimetype.split('/')[1]
+      const changedName = (new Date().getTime()) + '.' + fileType;
       cb(null, changedName);
     }
   })
 });
 
-// 返回html页面
+// 首页
 app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, './views/index.html'));
 })
 
 
 // 图片上传
-app.post('/upload', upload.single('files'), (req, res) => {
-  console.log(req.file);
-  res.json({
-    code: 0,
-    data: req.file.path,
-    msg: '上传成功'
+let singleUpload = upload.single('files');
+app.post('/upload', (req, res) => {
+  singleUpload(req, res, (err) => {
+    if (err || err instanceof multer.MulterError) {
+      // limit中限制的
+      res.json({
+        code: 1,
+        data: '',
+        msg: err.message
+      })
+    } else {
+      res.json({
+        code: 0,
+        data: req.file.path,
+        msg: '上传成功'
+      })
+    }
   })
 })
 
--- readme.md
+++ readme.md
@@ -1,1 +1,19 @@
 # node + vue 实现的文件上传
+
+
+## 依赖
+
+后端
+使用`express`搭建node服务器
+使用`body-parser`解析前端post传递的数据
+使用`multer`中间件保存图片
+
+前端
+使用`axios`发起http请求
+
+
+
+
+
+
+
